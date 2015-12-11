47e52d0eb7336315b1f31fd341d7f358698376db
salangkar acharya <noway@salangkars-MacBook-Pro.local>
2015-12-11 15:41:18 +1100

    initial push

---
+++ src/com/restapi/Initializer.java
@@ -0,0 +1,10 @@
+package com.restapi;
+
+public class Initializer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/restapi/contact.java
@@ -0,0 +1,5 @@
+package com.restapi;
+
+public class contact {
+
+}
